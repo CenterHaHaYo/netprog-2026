8c2357184eedf4137fe9fded738c284897b7a6f8
CenterHaHaYo <center29138@gmail.com>
2026-09-04 21:00:33 +0700

    Initial commit

---
+++ lab01_setup/connect_demo.py
@@ -0,0 +1,7 @@
+# connect_demo.py
+import socket, time
+
+s = socket.create_connection(("www.python.org", 443))
+print("Connected:", s.getpeername())
+time.sleep(5) # keep it open so you can inspect it
+s.close()
